83ca5a9feeeb98e6936b5ccce64bb00685629e1b
Vastier <44627659+Vastier@users.noreply.github.com>
2020-02-07 20:23:05 -0300

    Update chocolateyinstall.ps1

--- Servidores/Liet Kynes/chocolateyinstall.ps1
+++ Servidores/Liet Kynes/chocolateyinstall.ps1
@@ -1,14 +1,17 @@
+$packageName= 'fusionagent'
+$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
+
+
 $packageArgs = @{
-  packageName            = 'googlechrome'
-  fileType               = 'MSI'
-  filelocation           = 'Join-Path $toolsDir'
-  file                   = 'googlechromestandaloneenterprise.msi'
-  file64                 = 'googlechromestandaloneenterprise64.msi'
-  checksum               = '3e420702abdc16cdaae89e3d53e3fa9d941d3c9365755f3abe382144d8f1174c'
-  checksum64             = '904e5b102d27199f7dc8e32d0c3f45df57b3747fa684bfdff54f450b85a638d7'
+  packageName            = 'fusionagent'
+  fileType               = 'exe'
+  file                   = "$toolsDir\fusioninventory-agent_windows-x86_2.5.2.exe"
+  file64                 = "$toolsDir\fusioninventory-agent_windows-x64_2.5.2.exe"
+  checksum               = 'b560945c5f7615300c48fe5c9305d268806b77168fa013dfea155a10b8f80ffd'
+  checksum64             = 'd2bc50020e4947ddb2fc03256bd3b6c423541daf498a4e00925cdaf6bb79431d'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
-  silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  silentArgs             = "/acceptlicense /runnow /server='' /S"
   validExitCodes         = @(0)
 }
 
